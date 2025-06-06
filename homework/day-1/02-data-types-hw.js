59cb95681a49a3921ba03ec30791cc2a73297d71
aramya1702 <59823139+aramya1702@users.noreply.github.com>
2025-06-06 23:56:06 +0530

    Add files via upload
    Day-1 homework on let and data types

---
+++ homework/day-1/02-data-types-hw.js
@@ -0,0 +1,9 @@
+let firstName ='Ramya', companyName = 'CTS',mobileNumber=1238940321,isAutomation=true,hasPlaywright;
+
+
+console.log( 'my name is '+firstName + ' and data type is ' + typeof(firstName) );
+console.log( 'my company name is '+companyName + ' and data type is ' + typeof(companyName) );
+console.log( 'my phonenumber is '+mobileNumber + ' and data type is ' + typeof(mobileNumber) );
+console.log( 'is automation ? '+isAutomation + ' and data type is ' + typeof(isAutomation) );
+console.log( 'hasplaywright!! '+hasPlaywright + ' and data type is ' + typeof(hasPlaywright) );
+
