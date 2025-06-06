a550bd8fe5daa3e96dd8974a3b0d26b4b2eee286
aramya1702 <59823139+aramya1702@users.noreply.github.com>
2025-06-07 00:07:02 +0530

    Add files via upload
    odd or even method

---
+++ homework/day-2/oddeven.js
@@ -0,0 +1,13 @@
+function isOddOrEven(v1) {
+    // let v1 = 12;
+    if (v1 % 2 == 0) {
+        console.log('the number is even');
+
+    }
+    else {
+        console.log('number is odd');
+
+    }
+
+}
+isOddOrEven(11)
